232130c3ed33b36417851296721241a0cb323f5e
SamanthaMiyahira <samanthamiyahira@usp.br>
2025-11-25 19:56:32 -0300

    docs: organizacao dos arquivos em pastas em nov25

---
+++ source/nov25/api/comparador_sem_tipo_logradouro/run_normalizacao.py
@@ -0,0 +1,111 @@
+# run_normalizacao.py
+import pandas as pd
+from datetime import datetime
+from normalizar import normalizar_datasets
+import time
+import os
+
+"""
+Run normalizacao
+Responsabilidades:
+1. Carregar arquivos de dataset.
+2. Normaliza colunas de logradouro, número, bairro e codigo_setor.
+4. Salvar o dataset normalizado em csv.
+"""
+
+# -------------------
+# Configurações do usuário - Dados de dataset
+# -------------------
+
+import argparse
+import unicodedata
+import re
+
+# normaliza o paramentro de dataset local_base_dados
+def normalizar(texto: str) -> str:
+    # remove acentos
+    texto = unicodedata.normalize('NFKD', texto).encode('ascii', 'ignore').decode('utf-8')
+    # tudo minúsculo
+    texto = texto.lower()
+    # troca qualquer sequência de espaços por underline
+    texto = re.sub(r'\s+', '_', texto)
+    return texto
+
+
+# le arquivo de dataset
+parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+
+parser.add_argument("--tipo_dados", type=str, required=True, help="se é entrada ou do cnefe")
+parser.add_argument("--local_base_dados", type=str, required=True, help="define do qual local é a base de dados, ex: Diadema")
+
+parser.add_argument("--arquivo_dataset", type=str, required=True, help="arquivo do dataset com endereços a ser geocodificados")
+
+parser.add_argument("--separador_arq_dataset", type=str, required=True, help="separador do arquivo do dataset")
+
+parser.add_argument("--colunas_logradouro_dataset", nargs="+", help="coluna de endereço do arquivo do dataset")
+
+parser.add_argument("--coluna_bairro_dataset", type=str, required=True, help="coluna de bairro do arquivo do dataset")
+
+parser.add_argument("--coluna_numero_dataset", type=str, required=True, help="coluna de numero do arquivo do dataset")
+
+parser.add_argument("--coluna_cd_setor_dataset", type=str, required=True, help="coluna com o setor censitario do arquivo do dataset")
+
+args = parser.parse_args()
+
+
+# coloca os argumentos nas variaveis
+
+# dados dos arquivos de dataset e do cnefe
+arquivo_dataset = args.arquivo_dataset
+
+sep_arq_dataset = args.separador_arq_dataset
+
+# colunas
+colunas_logradouro_dataset = args.colunas_logradouro_dataset
+
+coluna_bairro_dataset = args.coluna_bairro_dataset
+
+coluna_numero_dataset = args.coluna_numero_dataset
+
+# dados do arquivo de dataset
+coluna_cd_setor_dataset = args.coluna_cd_setor_dataset
+
+# variaveis para nome de arquivos finais
+base_dados = normalizar(args.local_base_dados) # local da base de dados para colocar no nome do arquivo final
+tipo_dados = normalizar(args.tipo_dados) # entrada ou cnefe
+
+# -------------------
+# Execução
+# -------------------
+
+start_time = time.time()
+
+# Carrega dados
+df = pd.read_csv(arquivo_dataset, sep=sep_arq_dataset, dtype=str)
+
+# Executa a comparação
+df_dataset_normalizado = normalizar_datasets(
+    df,
+    colunas_logradouro=colunas_logradouro_dataset,
+    col_bairro=coluna_bairro_dataset,
+    col_num=coluna_numero_dataset,
+    cd_setor=coluna_cd_setor_dataset
+)
+
+# -------------------
+# Exporta CSV
+# -------------------
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# Cria pasta de resultados separada pelo algoritmo
+pasta_resultados = "datasets_normalizados"
+os.makedirs(pasta_resultados, exist_ok=True)
+
+# Gera os nomes dos arquivos
+arquivo_dataset_normalizada = os.path.join(pasta_resultados, f"normalizado_{base_dados}_{tipo_dados}.csv")
+
+# Salva CSV
+df_dataset_normalizado.to_csv(arquivo_dataset_normalizada, index=False, sep=";") # ; será o separador dos arquivos normalizados
+elapsed = time.time() - start_time
+print(f"CSV do dataset normalizado salvo em: {arquivo_dataset_normalizada}")
+print(f"Tempo de execução: {elapsed:.2f} segundos.")
